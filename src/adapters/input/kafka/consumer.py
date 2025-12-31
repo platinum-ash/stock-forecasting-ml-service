ce474de87a5769d46c8fae3939d946aa4b6f5ad6
Asharab <haider.asharab@gmail.com>
2025-12-31 21:46:56 +0100

    Read Kafka topics from env vars

--- src/adapters/input/kafka/consumer.py
+++ src/adapters/input/kafka/consumer.py
@@ -1,6 +1,7 @@
 """
 Kafka consumer - Input adapter for preprocessing completion events.
 """
+import os
 import json
 import logging
 from typing import Optional
@@ -22,7 +23,7 @@ class ForecastingConsumer:
     ):
         self.bootstrap_servers = bootstrap_servers
         self.event_handler = event_handler
-        self.topic = topic
+        self.topic = os.getenv("KAFKA__INPUT_TOPIC", topic)
         self.group_id = group_id
         self.consumer: Optional[AIOKafkaConsumer] = None
         self.is_running = False
